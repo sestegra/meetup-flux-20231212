91c8fbf301b7b9857b5f54d1ef31eb02a72314f9
Stéphane Este-Gracias <sestegra@gmail.com>
2023-12-14 10:29:01 +0100

    Fix ordering
    Signed-off-by: Stéphane Este-Gracias <sestegra@gmail.com>

--- 00-reset.sh
+++ 00-reset.sh
@@ -14,3 +14,7 @@ kubectl create ns podinfo-helm
 
 # Delete all pods in ingress-nginx namespace
 kubectl delete pod -n ingress-nginx -l app.kubernetes.io/name=ingress-nginx
+
+# Create a root CA for the cluster
+flux create secret tls labs-root-ca \
+  --ca-crt-file=/etc/ssl/certs/labs.pem
